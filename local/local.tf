680bd3ef0b7f278e5e714cb0185f2a4c8549bcea
Sanchitha Udana Wijesundara <udanasanchitha4@gmail.com>
2025-11-09 14:29:45 +0530

    tf

--- local/local.tf
+++ local/local.tf
@@ -2,6 +2,7 @@ resource "local_file" "local-demo-1" {
 
     filename = "/Users/apple/Desktop/terraform-practice/data.txt"
     content = "Welcome to Terraform IAC"
+    file_permission = "0700" # user full access
   
 }
 
