5860f205394b33e548aa838051756d5c8d897bc2
Sanchitha Udana Wijesundara <udanasanchitha4@gmail.com>
2025-11-10 16:42:57 +0530

    terraform output

--- variables/main.tf
+++ variables/main.tf
@@ -3,3 +3,13 @@ resource "local_file" "variable" {
     content = var.variable.content
   
 }
+
+resource "local_file" "welcome" {
+    filename = "./welcome.txt"
+    content = "Welcome To KodeKloude"
+}
+
+# terraform output
+output "welcome_message" {
+  value = local_file.welcome.content
+}
